c1091252916ac41463a1f1d400db872cb7a97e05
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-10-04 09:56:36 +0300

    adaug medicamentul la reteta cu javascript si merge(lol)

--- app/controllers/reteta_controller.rb
+++ app/controllers/reteta_controller.rb
@@ -2,6 +2,10 @@ class RetetaController < InheritedResources::Base
   respond_to :html, :js
   belongs_to :pacient
   #o reteta trebuie sa aibe mai multe medicamente
+  def index
+    @pacient = Pacient.find(params[:pacient_id])
+    @retetum = pacient.reteta.all
+  end
   def show
     @pacient = Pacient.find(params[:pacient_id])
     @retetum = Retetum.find(params[:id])
@@ -13,6 +17,13 @@ class RetetaController < InheritedResources::Base
       #end
     end
   end
+  def create
+    @pacient = Pacient.find(params[:pacient_id])
+    @retetum = @pacient.reteta.new(params[:retetum])
+    @retetum.save
+    #nu ajunge aici
+    respond_with @pacient #@retetum, :location => pacient_retetum_url
+  end
   def new
     respond_to do |format|
       @pacient = Pacient.find(params[:pacient_id])
@@ -23,6 +34,11 @@ class RetetaController < InheritedResources::Base
                                               :type => "application/pdf", :page_size => "A4" 
       end
     end
+  end
+  def medicamente
+    @pacient = Pacient.find(params[:pacient_id])
+    @retetum = Retetum.find(params[:id])
+    medicamente = @retetum.medicamente_id
   end
   def update_pastile
     #update de pastile in functie de ce clasa selecteaza
